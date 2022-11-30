234f092b1733ed266090cbb45968a23abc49a035
kfc0105 <kentam633@gmail.com>
2022-11-29 21:27:03 -0800

    Assignment 4 complete

---
+++ fancifymytext.js
@@ -0,0 +1,24 @@
+function bigger(){
+    //alert("Hello, World! I am using Javascript!");    
+    document.getElementById("text1").style.fontSize = "24pt";
+}
+
+function fancyShmancy(){
+    //alert("Hello, World! I am using Javascript!");  
+    document.getElementById("text1").style.fontWeight = "bold";
+    document.getElementById("text1").style.color = "blue";
+    document.getElementById("text1").style.textDecoration = "underline";
+}
+
+function boringBetty(){
+    document.getElementById("text1").style.fontWeight = "normal";
+    document.getElementById("text1").style.color = "black";
+    document.getElementById("text1").style.textDecoration = "";
+}
+
+function moo(){
+    document.getElementById("text1").style.textTransform = "uppercase"
+    var str = document.getElementById("text1").value;
+    var parts = str.split(".");
+    document.getElementById("text1").value = parts.join(" -MOO");
+}
